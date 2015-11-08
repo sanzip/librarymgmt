50a7677a207608e013515b9f157a5bef87bf4884
koolzephyr <sameer.koirala@deerwalk.edu.np>
2015-11-08 13:59:58 +0545

    Created new domain "BookInfo"

---
+++ DwitLibrary/grails-app/domain/np/edu/dwit/BookInfo.groovy
@@ -0,0 +1,27 @@
+package np.edu.dwit
+
+/**
+ * Created by zephyr on 11/8/15.
+ */
+class BookInfo {
+    Book book
+    Integer bookNumber
+    String edition
+    String publishedYear
+    String pages
+    Double cost
+    String source
+
+    static constraints = {
+        book blank: false, nullable: false
+        bookNumber blank: false, nullable: false
+        edition blank: false, nullable: false
+        publishedYear blank: false, nullable: false
+        pages blank: false, nullable: false
+        cost blank:false, nullable: false
+        source blank: false, nullable: false
+    }
+    static mapping = {
+        version false
+    }
+}
