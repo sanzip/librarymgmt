b953fad2dab2247db5823a7eab3a7f9a497b9c9b
pratik1919 <pratik.budhathoki@deerwalk.edu.np>
2015-11-09 17:47:34 +0545

    Updated Book.groovy.  availableQuantity nullable is set to true.

--- DwitLibrary/grails-app/domain/np/edu/dwit/Book.groovy
+++ DwitLibrary/grails-app/domain/np/edu/dwit/Book.groovy
@@ -12,6 +12,7 @@ class Book {
         name blank: false, nullable: false
         author blank: false, nullable: false
         publication blank: false, nullable: false
+
         availableQuantity nullable: true
         totalQuantity nullable: false
         bookType nullable: false, blank: false, inList: ['Reference', 'Gifted', 'Borrowable', 'Novel']
