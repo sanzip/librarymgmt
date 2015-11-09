e41c37884f1a509609669e4bd894ec10a1be3af8
AnilShrestha <wedeerwalker@gmail.com>
2015-11-09 11:15:49 +0545

    Book domain changes and Book Controller changes

--- DwitLibrary/grails-app/domain/np/edu/dwit/Member.groovy
+++ DwitLibrary/grails-app/domain/np/edu/dwit/Member.groovy
@@ -4,12 +4,14 @@ class Member extends User {
     String email
     String contact
     String status
+    String fullName
 
     static constraints = {
 
         email nullable: true, blank: true
         contact nullable: true, blank: true
         status nullable: true, blank: true
+        fullName nullable: true,blank: true
     }
 
     Member(String username, String password) {
