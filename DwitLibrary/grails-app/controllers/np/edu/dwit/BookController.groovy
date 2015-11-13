f605fb2835f336e35c1e9ed62d61499cd022141a
koolzephyr <sameer.koirala@deerwalk.edu.np>
2015-11-13 19:38:03 +0545

    Initialized totalQuantity and availableQuantity to 0

--- DwitLibrary/grails-app/controllers/np/edu/dwit/BookController.groovy
+++ DwitLibrary/grails-app/controllers/np/edu/dwit/BookController.groovy
@@ -50,6 +50,7 @@ class BookController {
             return
         }
 
+        bookInstance.totalQuantity=0
         bookInstance.availableQuantity=bookInstance.totalQuantity
         bookInstance.save flush: true
 
