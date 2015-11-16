7fb9edbc739554f9ad3eb53f784e275fa384827b
koolzephyr <sameer.koirala@deerwalk.edu.np>
2015-11-16 14:59:06 +0545

    Redirect to book list after update

--- DwitLibrary/grails-app/controllers/np/edu/dwit/BookInfoController.groovy
+++ DwitLibrary/grails-app/controllers/np/edu/dwit/BookInfoController.groovy
@@ -141,7 +141,7 @@ class BookInfoController {
         }
 
         bookInfoInstance.save flush:true
-        redirect(controller: params.currentController, action:'index',params:[id:bookInfoInstance.id])
+        redirect(controller: 'book', action:'index',params:[id:bookInfoInstance.id])
 
 /*        request.withFormat {
             form {
