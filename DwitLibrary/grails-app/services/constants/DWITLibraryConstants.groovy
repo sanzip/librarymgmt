4bb6c61a99baea1be782b6344a86d0abbd9fc00f
koolzephyr <sameer.koirala@deerwalk.edu.np>
2015-11-25 17:55:56 +0545

    Made changes to fine constant

--- DwitLibrary/grails-app/services/constants/DWITLibraryConstants.groovy
+++ DwitLibrary/grails-app/services/constants/DWITLibraryConstants.groovy
@@ -5,15 +5,15 @@ package constants
  */
 class DWITLibraryConstants {
 
-    public static final double FINE_AMT_STUDENT = 5.0;
+    public static final double FINE_AMT_STUDENT = 2.0;
     public static final double FINE_AMT_FACULTY = 10.0;
     public static final double FINE_AMT_ADMIN = 10.0;
-    public static final double FINE_AMT_LIBRARIAN =10.0;
-    public static final short COURSE_BOOK_BORROWABLE_STUDENT = 7;
+    public static final double FINE_AMT_LIBRARIAN = 2.0;
+    public static final short COURSE_BOOK_BORROWABLE_STUDENT = 15;
     public static final short COURSE_BOOK_BORROWABLE_FACULTY = 180;
-    public static final short COURSE_BOOK_BORROWABLE_ADMIN = 30;
-    public static final short COURSE_BOOK_BORROWABLE_LIBRARIAN = 30;
-    public static final short NOVEL_BOOK_BORROWABLE = 15;
+    public static final short COURSE_BOOK_BORROWABLE_ADMIN = 15;
+    public static final short COURSE_BOOK_BORROWABLE_LIBRARIAN = 15;
+    public static final short NOVEL_BOOK_BORROWABLE = 21;
     public static final short LIMIT_BOOK_BORROWABLE_STUDENT = 3;
     public static final short LIMIT_BOOK_BORROWABLE_ADMIN = 3;
     public static final short LIMIT_BOOK_BORROWABLE_LIBRARIAN = 3;
