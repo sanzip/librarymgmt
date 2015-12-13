11db360d703a6ed00991dce49fadde8a872d8485
koolzephyr <sameer.koirala@deerwalk.edu.np>
2015-12-13 07:09:01 +0545

    Changed update and moved logics from controller to service

--- DwitLibrary/grails-app/controllers/np/edu/dwit/MemberController.groovy
+++ DwitLibrary/grails-app/controllers/np/edu/dwit/MemberController.groovy
@@ -14,25 +14,21 @@ class MemberController {
     def memberService
     @Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
     def index() {
-        println(springSecurityService.isLoggedIn())
         if (springSecurityService.loggedIn) {
-            def role = springSecurityService.getAuthentication().authorities.toString()
-            def currentUser = User.findById(springSecurityService.principal.id)
+            def currentUser = memberService.getCurrentUser()
             session["userName"] = currentUser.username
             redirect(controller:'Member', action:'dashboard')
         }
         else {
             redirect(controller:'login', action:'index')
         }
-
     }
 
     @Secured(['IS_AUTHENTICATED_FULLY'])
     def dashboard() {
 
-        def currentUser= Member.findById(springSecurityService.principal.id)
-        def count = Borrow.findAllByMemberAndReturned(currentUser, false)
-        def bookList = Book.list();
+        def count = memberService.getBorrowedBookCount()
+        def bookList = memberService.getBookList()
         render (view: "dashboard", model:[list: bookList, listCount:bookList.size(), count: (3-count.size())])
     }
 
@@ -41,11 +37,12 @@ class MemberController {
     def history(){
 
         def currentUser = springSecurityService.currentUser
-        def member = Member.findByUsername(currentUser)
+        def member = memberService.getCurrentUser()
         if(!member.username.equalsIgnoreCase(currentUser.username)){
             flash.message = "Sorry, you're not authorized to view this page."
             redirect(action: 'index')
         }
+
         [history : memberService.getHistory(member)]
     }
 
@@ -56,22 +53,7 @@ class MemberController {
         String query = params?.query
         def states =  params?.states
         states = states.split(',')
-        if (query.length() > 0) {
-            def probableMembers = Member.findAllByFullNameIlike(query + '%')
-            JSONArray jsonArray = new JSONArray()
-            int i = 0
-            for(Member member : probableMembers) {
-                JSONObject jsonObject = new JSONObject()
-                if(!states.contains(member.fullName)) {
-                    jsonObject.put("fullName", member.fullName)
-                    jsonArray.add(jsonObject)
-                }
-                if(i++ == max)
-                    break;
-            }
-            if(jsonArray.size() > 0)
-                render jsonArray.toJSONString()
-        }
+        render memberService.searchMember(query,states,max)
     }
 
     @Secured("ROLE_LIBRARIAN")
@@ -94,53 +76,17 @@ class MemberController {
     @Secured("ROLE_LIBRARIAN")
     @Transactional
     def save(Member memberInstance) {
-        if (memberInstance == null) {
-            notFound()
-            return
+        def message = memberService.saveMember(memberInstance,params.role,true)
+        if(message.equals('success')){
+            flash.message ='New User created'
+            redirect( action: 'list' ,params: [messageType: 'success'])
         }
+        else{
+            message=message.split(',')
+            flash.message = message[0]
+            respond memberInstance, view: 'create', params: [messageType: message[1]]
 
-        if (memberInstance.hasErrors()) {
-            respond memberInstance.errors, view: 'create'
-            return
         }
-        memberInstance.fullName=memberInstance.fullName/*+"-"+memberInstance.userId*/ /*>>>>This code creates issue*/
-
-        def fName = memberInstance.fullName.toLowerCase().split(" ")
-        memberInstance.username=fName[0]+"_"+memberInstance.userId
-        params.role
-
-        def bodyOfEmail = "\nHello $memberInstance.fullName,\n\nYour account has been created on Library Management System.\n\nYour credentials are: \n" +
-                "\n\tUsername: $memberInstance.username\n\tPassword: $memberInstance.password\nYou can change this password right after you login.\n\nThanks,\nThe DWIT Library.";
-
-        if (!memberInstance.save(flush: true,failOnError: true)) {
-            flash.message ='Unable to save new User'
-            def messageType = 'success'
-            render view: "create", model: [userInstance: memberInstance], params: [messageType: messageType]
-            return
-        }
-        if  (!memberInstance.authorities.contains( Role.findByAuthority(params.role))) {
-            UserRole.create memberInstance, Role.findByAuthority(params.role)
-        }
-
-        sendMail {
-
-            async true
-            to memberInstance.email
-            subject "LMS: User Credentials"
-            text bodyOfEmail
-        }
-        flash.message ='New User saved'
-        def messageType = 'success'
-        redirect( action: 'list' ,params: [messageType: messageType])
-        /*request.withFormat {
-            form {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'memberInstance.label', default: 'Member'), memberInstance.id])
-                redirect memberInstance
-
-            }
-            '*' { respond memberInstance, [status: CREATED] }
-
-        }*/
     }
 
     @Secured("ROLE_LIBRARIAN")
@@ -156,67 +102,34 @@ class MemberController {
     @Secured("IS_AUTHENTICATED_FULLY")
     @Transactional
     def updatePassword(){
-        def memberInstance = User.findById(springSecurityService.principal.id)
-        if (memberInstance == null) {
-            notFound()
-            return
-        }
-        def password = params.password
-        def newPassword = params.npassword
-        def newPassword2= params.rpassword
-        def messageType
+        def member = memberService.getCurrentUser()
 
-        if (!password || !newPassword || !newPassword2 || newPassword != newPassword2) {
-            flash.message = 'Please enter your current password and a valid new password'
-            messageType = 'error'
-            redirect( action: 'editPassword', params: [messageType: messageType])
-            return
-        }
+        def status=memberService.validPassword(member,params.password,params.npassword,params.rpassword)
 
-        if (!springSecurityService.passwordEncoder.isPasswordValid(memberInstance.password, password, null /*salt*/)) {
-            flash.message =  'Current password is incorrect'
-            messageType = 'error'
-            redirect( action: 'editPassword', params: [messageType: messageType])
-            return
+        if(status.equals('valid')){
+            status=memberService.updatePassword(member,params.npassword)
+            status = status.split(',')
+            flash.message = status[0]
+            redirect( action: 'dashboard', params: [messageType: status[1]])
         }
-
-        if (springSecurityService.passwordEncoder.isPasswordValid(memberInstance.password, newPassword, null /*salt*/)) {
-            flash.message = 'Please choose a different password from your current one'
-            messageType = 'error'
-            redirect( action: 'editPassword', params: [messageType: messageType])
-            return
+        else {
+            flash.message = status
+            redirect( action: 'editPassword', params: [messageType: 'error'])
         }
-        memberInstance.password = newPassword
-        memberInstance.save flush: true, failOnError: true
-        flash.message = 'Password changed.'
-        messageType = 'success'
-        redirect( action: 'dashboard', params: [messageType: messageType])
     }
     @Secured("ROLE_LIBRARIAN")
     @Transactional
     def update(Member memberInstance) {
-        if (memberInstance == null) {
-            notFound()
-            return
+        def message = memberService.saveMember(memberInstance,params.role,false)
+        if (message.equals('success')){
+            flash.message ='User updated'
+            redirect( action: 'list' ,params: [messageType: 'success'])
         }
-
-        if (memberInstance.hasErrors()) {
-            respond memberInstance.errors, view: 'edit'
-            return
+        else{
+            message=message.split(',')
+            flash.message = message[0]
+            respond memberInstance, view: 'edit', params: [messageType: message[1]]
         }
-
-        memberInstance.save flush: true, failOnError: true
-        flash.message = 'User updated'
-        def messageType = 'success'
-        redirect action:  'list' ,params: [messageType: messageType]
-
-//        request.withFormat {
-//            form {
-//                flash.message = message(code: 'default.updated.message', args: [message(code: 'Member.label', default: 'Member'), memberInstance.id])
-//                redirect memberInstance
-//            }
-//            '*' { respond memberInstance, [status: OK] }
-//        }
     }
 
     @Secured("ROLE_LIBRARIAN")
--- DwitLibrary/grails-app/services/MemberService.groovy
+++ DwitLibrary/grails-app/services/MemberService.groovy
@@ -1,12 +1,130 @@
 
 
 import grails.transaction.Transactional
+import np.edu.dwit.Book
 import np.edu.dwit.Borrow
 import np.edu.dwit.Fine
 import np.edu.dwit.Member
+import np.edu.dwit.Role
+import np.edu.dwit.User
+import np.edu.dwit.UserRole
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
 
 @Transactional
 class MemberService {
+   def springSecurityService
+   def getCurrentUser(){
+       return User.findById(springSecurityService.principal.id)
+   }
+    def getBorrowedBookCount(){
+        def currentUser= getCurrentUser()
+        return Borrow.findAllByMemberAndReturned(currentUser, false)
+    }
+
+    def getBookList(){
+        return Book.list();
+    }
+
+    def getProbableMembers(def query){
+        return Member.findAllByFullNameIlike(query + '%')
+    }
+
+    def searchMember(String query, def states, def max){
+        if (query.length() > 0) {
+            def probableMembers = getProbableMembers(query)
+            JSONArray jsonArray = new JSONArray()
+            int i = 0
+            for(Member member : probableMembers) {
+                JSONObject jsonObject = new JSONObject()
+                if(!states.contains(member.fullName)) {
+                    jsonObject.put("fullName", member.fullName)
+                    jsonArray.add(jsonObject)
+                }
+                if(i++ == max)
+                    break;
+            }
+            if(jsonArray.size() > 0) {
+                return jsonArray.toJSONString()
+            }
+        }
+    }
+    def mailMember(Member memberInstance){
+        def bodyOfEmail = "\nHello $memberInstance.fullName,\n\nYour account has been created on GyanSangalo.\n\nYour credentials are: \n" +
+                "\n\tUsername: $memberInstance.username\n\tPassword: $memberInstance.password\nYou can change this password right after you login.\n\nThanks,\nThe DWIT Library.";
+
+
+        sendMail {
+
+            async true
+            to memberInstance.email
+            subject "GyanSangalo: User Credentials"
+            text bodyOfEmail
+        }
+    }
+    def saveMember(Member memberInstance, String role, boolean newMember){
+        def member = Member.findByUserId(memberInstance.userId)
+        if (member&&newMember){
+            return 'User Id already exists,error'
+        }
+        else {
+            def  fullName
+            if(newMember){
+                fullName=memberInstance.fullName
+                memberInstance.fullName=fullName+" -"+memberInstance.userId
+                def fName = fullName.toLowerCase().split(' ')
+                memberInstance.username=fName[0]+"_"+memberInstance.userId
+            }
+            else if(!newMember){
+                fullName=memberInstance.fullName.split(' ')
+                memberInstance.fullName=fullName[0]+' '+fullName[1]+' -'+memberInstance.userId
+                memberInstance.username=fullName[0]+'_'+memberInstance.userId
+            }
+
+            if (!memberInstance.save(flush: true,failOnError: true)) {
+                return 'Unable to save new User,error'
+            }
+            if  (!memberInstance.authorities.contains( Role.findByAuthority(role))) {
+                if (!newMember){
+                    def userRole = UserRole.findByUser(memberInstance)
+                    userRole.delete flush: true
+                }
+                UserRole.create memberInstance, Role.findByAuthority(role)
+
+            }
+            if(newMember){
+                mailMember(memberInstance)
+            }
+            return 'success'
+        }
+    }
+
+    def validPassword(def member, def password, def newPassword, def newPassword2)
+    {
+        if (!springSecurityService.passwordEncoder.isPasswordValid(member.password, password, null /*salt*/)) {
+            return 'Invalid current password'
+        }
+
+        if (newPassword != newPassword2) {
+            return 'New password doesn\'t match'
+        }
+
+        if (springSecurityService.passwordEncoder.isPasswordValid(member.password, newPassword, null /*salt*/)) {
+            return 'New password is same as current password'
+        }
+        return 'valid'
+    }
+
+
+    def updatePassword(def member, def newPassword){
+        member.password = newPassword
+        if(!member.save(flush: true, failOnError: true)){
+            return 'Unable to change password,error'
+        }
+        else {
+            return 'Password changed,success'
+        }
+    }
 
     def getHistory(Member member) {
 
